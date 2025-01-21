2442ede03d779393837bcc58bc5f547a5fc3f646
somkuan <somkuan@gmail.com>
2025-01-21 22:53:43 +0700

    4 - Setup Nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Somkuan</Text>
+    <View className="font-bold" >
+      <Text className="text-red-500">Somkuan</Text>
     </View>
   );
 }
